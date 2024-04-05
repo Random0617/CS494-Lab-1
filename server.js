2ec9cd7a101d671cf29c6b0d99b719d6d0a91a10
Tran Thuc Minh Tri <minhtri170603@gmail.com>
2024-04-05 13:12:24 +0700

    Enter username to send to server
    To do next: Update README.md, checks for allowed characters in usernames, do not allow registered players to enter their name again

--- server.js
+++ server.js
@@ -1,27 +1,53 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-let number = 0;
+let connectedUsers = 0;
+let registeredPlayers = 0;
+let player_list = [];
 
-app.use(express.static('public'));
+io.on("connection", (socket) => {
+  connectedUsers++;
+  io.emit("userCount", connectedUsers);
+  io.emit("update player list", {
+    list: player_list,
+    length: player_list.length,
+  });
 
-io.on('connection', (socket) => {
-  // Emit the current number to the client
-  socket.emit('updateNumber', number);
-  
-  // Handle button click from any client
-  socket.on('incrementNumber', () => {
-    number++;
-    // Broadcast the updated number to all clients
-    io.emit('updateNumber', number);
+  socket.on("disconnect", () => {
+    connectedUsers--;
+    io.emit("userCount", connectedUsers);
+  });
+
+  socket.on("check username", (msg) => {
+    player_list.push(msg);
+    console.log("Username submitted: " + msg);
+    io.emit("update player list", {
+      list: player_list,
+      length: player_list.length,
+    });
   });
 });
 
+/*
+function checkUsername() {
+    var username = document.getElementById("usernameInput").value;
+    var regex = /^[a-zA-Z0-9_]{1,10}$/;
+    if (regex.test(username)) {
+        document.getElementById("resultMessage").innerHTML = "Registration completed";
+    } else {
+        document.getElementById("resultMessage").innerHTML = "Choose another username";
+    }
+}
+*/
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
 server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
